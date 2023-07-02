d5f96ae37d558f669cb8534eac1c2b40f4e45cac
atrem.bezruk <artem.bezruk@gmail.com>
2023-07-02 07:15:06 +0300

    Seminar#1 home work

---
+++ 01_Intro/homeWork/hw_01_task/src/Main.java
@@ -0,0 +1,27 @@
+// Курс: Java. Объектно-ориентированное программирование
+// Урок 1. Принципы ООП: Инкапсуляция, наследование, полиморфизм
+// Домашняя работа
+
+//Задание
+// Реализовать, с учетом ооп подхода, приложение для проведения исследований с генеалогическим древом.
+// Идея: описать некоторое количество компонент, например: модель человека и дерева
+// Под “проведением исследования” можно понимать например получение всех детей выбранного человека.
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Ctrl+D to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Cmd+F8.
+            System.out.println("i = " + i);
+        }
+    }
+}
